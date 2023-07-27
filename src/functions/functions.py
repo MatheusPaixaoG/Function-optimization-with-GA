f847e9c68b5046a5d7be95908bf630e45af38eb4
bruhenri12 <bhsm@cin.ufpe.br>
2023-07-26 22:05:22 -0300

    Addind classes

--- src/functions/functions.py
+++ src/functions/functions.py
@@ -1,12 +1,14 @@
 import math
+import matplotlib.pyplot as plt
 import numpy as np
 
 class Functions:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, points_limit, lower_limit, upper_limit):
+        self.points_limit = points_limit
+        self.lower_limit = lower_limit
+        self.upper_limit = upper_limit
 
-    def ackley(point, a=20, b=0.2, c=2*np.pi):
-        # point is the vector/tuple of coordinates
+    def ackley(self, point, a=20, b=0.2, c=2*np.pi):
         d = len(point) 
         # First, we calculate the summation terms
         fst_sum = sum(np.multiply(point, point))
@@ -18,11 +20,63 @@ class Functions:
         result = srqt_term + cos_term + a + np.exp(1)
         return result
 
-    def rastrigin(x):
-        return 10 * len(x) + sum([(i ** 2) - 10 * math.cos(2 * math.pi * i) for i in x])
+    def rastrigin(self, point):
+        return 10 * len(point) + sum([(i ** 2) - 10 * math.cos(2 * math.pi * i) for i in point])
 
-    def schwefel(x):
-        return 418.9829 * len(x) - sum([i * math.sin(math.sqrt(abs(i))) for i in x])
+    def schwefel(self, point):
+        return 418.9829 * len(point) - sum([i * math.sin(math.sqrt(abs(i))) for i in point])
 
-    def rosenbrock(x):
-        return sum([100 * (x[x.index(i)+1] - i ** 2) ** 2 + (i - 1) ** 2 for i in x])
+    def rosenbrock(self, point):
+
+        sum_list = []
+        for i in range(len(point)-1):
+            sum_list.append(100 * (point[i+1] - point[i] ** 2) ** 2 + (point[i] - 1) ** 2)
+        return sum(sum_list)
+    
+    def plot(self, function):
+        fig = plt.figure(figsize=(40,40))
+        ax = fig.add_subplot(projection='3d')
+
+        # Make data.
+        y_range = [np.random.uniform(self.lower_limit,self.upper_limit) for y in range(self.points_limit)]
+        x_range = [np.random.uniform(self.lower_limit,self.upper_limit) for x in range(self.points_limit)]
+        z_list = [function((zipped[0],zipped[1])) for zipped in zip(x_range,y_range)]
+
+        ax.scatter3D(x_range,y_range,z_list,c=z_list, cmap='jet', alpha=1, s=50)
+        plt.title("WOW Look at Him!", size=18)
+        plt.xlabel('x-axis')
+        plt.ylabel('y-axis')
+        ax.set_zlabel('z-axis')
+        plt.show()
+
+
+class Ackley(Functions):
+    def __init__(self, points_limit, lower_limit, upper_limit):
+        super().__init__(points_limit, lower_limit, upper_limit)
+    
+    def plot(self):
+        return super().plot(super().ackley)
+
+
+class Rastrigin(Functions):
+    def __init__(self, points_limit, lower_limit, upper_limit):
+        super().__init__(points_limit, lower_limit, upper_limit)
+    
+    def plot(self):
+        return super().plot(super().rastrigin)
+    
+
+class Schwefel(Functions):
+    def __init__(self, points_limit, lower_limit, upper_limit):
+        super().__init__(points_limit, lower_limit, upper_limit)
+    
+    def plot(self):
+        return super().plot(super().schwefel)
+    
+
+class Rosenbrock(Functions):
+    def __init__(self, points_limit, lower_limit, upper_limit):
+        super().__init__(points_limit, lower_limit, upper_limit)
+    
+    def plot(self):
+        return super().plot(super().rosenbrock)
