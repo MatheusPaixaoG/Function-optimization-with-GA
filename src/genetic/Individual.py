8020a670a2646ff9bd5ee18a4485ba25c714431f
Matheus Paixão <mpgs@cin.ufpe.br>
2023-07-28 14:06:28 -0300

    Added population sort by fitness

--- src/genetic/Individual.py
+++ src/genetic/Individual.py
@@ -7,16 +7,17 @@ import numpy as np
 
 
 class Individual:
-    def __init__(self, lo_range, hi_range, gene=None):
+    def __init__(self, lo_range, hi_range, function_ID, gene=None):
         if gene == None:
             self.gene = np.round(np.random.uniform(lo_range, hi_range, 30), 3)
         else:
             self.gene = gene
         self.fitness_function = FitnessFunction(lo_range, hi_range)
+        self.fitness = round(self.calc_fitness(function_ID), 4)
 
-    def fitness(self, function_ID):
+    def calc_fitness(self, function_ID):
         fit = self.fitness_function.calculate(self.gene, function_ID)
         return round(fit, 4)
 
     def __str__(self):
-        return f"[Individual] {self.gene}"
+        return f"[Individual] {self.gene} {self.fitness}"
--- src/main.py
+++ src/main.py
@@ -2,11 +2,16 @@ from functions.FitnessFunction import Functions
 from genetic.Individual import Individual
 
 
-def init_population(population_size, lower_limit, upper_limit):
+def init_population(population_size, lower_limit, upper_limit, function_ID):
     population = [
-        Individual(lower_limit, upper_limit) for indiv in range(population_size)
+        Individual(lower_limit, upper_limit, function_ID)
+        for indiv in range(population_size)
     ]
     return population
+
+
+def sort_by_fitness(population):
+    population.sort(key=lambda x: x.fitness)
 
 
 if __name__ == "__main__":
@@ -18,11 +23,16 @@ if __name__ == "__main__":
     ros_low, ros_up = -5, 10
 
     population_size = 10
-    population = init_population(population_size, ack_low, ack_up)
+    population = init_population(population_size, ack_low, ack_up, Functions.ACKLEY)
 
-    for i in range(0, population_size):
-        ind = population[i]
-        print(f"{ind} {round(ind.fitness(Functions.ACKLEY), 4)}")
-        print(f"{ind} {round(ind.fitness(Functions.RASTRIGIN), 4)}")
-        print(f"{ind} {round(ind.fitness(Functions.ROSENBROCK), 4)}")
-        print(f"{ind} {round(ind.fitness(Functions.SCHWEFEL), 4)}")
+    # for i in range(0, population_size):
+    #     ind = population[i]
+    #     print(f"{ind} {round(ind.calc_fitness(Functions.ACKLEY), 4)}")
+    #     print(f"{ind} {round(ind.calc_fitness(Functions.RASTRIGIN), 4)}")
+    #     print(f"{ind} {round(ind.calc_fitness(Functions.ROSENBROCK), 4)}")
+    #     print(f"{ind} {round(ind.calc_fitness(Functions.SCHWEFEL), 4)}")
+
+    sort_by_fitness(population)
+
+    for indiv in population:
+        print(f"{indiv}")
