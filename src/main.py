44a249f17ed94a8541ad4897fde7184d95b2605f
Matheus Paixão <mpgs@cin.ufpe.br>
2023-07-28 11:21:28 -0300

    Adding population initialization

--- src/main.py
+++ src/main.py
@@ -1,6 +1,14 @@
 from functions.FitnessFunction import Functions
 from genetic.Individual import Individual
 
+
+def init_population(population_size, lower_limit, upper_limit):
+    population = [
+        Individual(lower_limit, upper_limit) for indiv in range(population_size)
+    ]
+    return population
+
+
 if __name__ == "__main__":
     points_limit = 50000
 
@@ -9,8 +17,11 @@ if __name__ == "__main__":
     schw_low, schw_up = -500, 500
     ros_low, ros_up = -5, 10
 
-    for i in range(0, 10):
-        ind = Individual(ack_low, ack_up)
+    population_size = 10
+    population = init_population(population_size, ack_low, ack_up)
+
+    for i in range(0, population_size):
+        ind = population[i]
         print(f"{ind} {round(ind.fitness(Functions.ACKLEY), 4)}")
         print(f"{ind} {round(ind.fitness(Functions.RASTRIGIN), 4)}")
         print(f"{ind} {round(ind.fitness(Functions.ROSENBROCK), 4)}")
