4f68ba781583caf9b105a85e770b9714891966dd
bruhenri12 <bhsm@cin.ufpe.br>
2023-08-07 10:37:57 -0300

    Auto creating data directory on /src path

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
+import os
 import params
-from genetic.GA_utils import run_ga
+
 from evolutive.EE_utils import run_ee
+from genetic.GA_utils import run_ga
 
 if __name__ == "__main__":
+
+    data_path = os.path.join(os.getcwd(),"data")
+
+    if (not os.path.exists(data_path)):
+        os.mkdir(data_path)
+
     params.set_function("ackley")
     points_limit = 50000
 
